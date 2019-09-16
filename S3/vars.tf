295bf779f981b48b0b53f24e6168c653fd441ccf
Patchaiappan Mandjiny <mandjiny89@gmail.com>
2019-09-16 09:00:09 -0700

    basic S3 bucket creation

--- S3/vars.tf
+++ S3/vars.tf
@@ -1,3 +1,7 @@
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_ID_KEY" {}
+
+# Below variable using a single string variable, we also have list, map and etc.
 variable "AWS_REGION" {
 	default = "eu-west-1"
 } 
