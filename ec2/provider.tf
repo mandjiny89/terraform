2a7e60a9e300a452f78449019435769c87dc7445
Patchaiappan Mandjiny <mandjiny89@gmail.com>
2019-09-19 08:26:12 -0700

    Setting up the AWS VPC infra using IGW and NAT gateway

--- ec2/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  version    = "~> 2.0"
-  region     = "${var.AWS_REGION}"
-  access_key = "${var.AWS_ACCESS_KEY_ID}"
-  secret_key = "${var.AWS_SECRET_ACCESS_ID_KEY}"
-}
--- ec2/resouces.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tff2-bucket"
-  acl    = "public-read"
-
-  tags = {
-    Name        = "testingtesting"
-    Environment = "Dev"
-  }
-}
